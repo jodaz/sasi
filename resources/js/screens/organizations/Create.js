dc46fc7b645a77f98f5092de24f1f47f6ab22922
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-01-23 13:51:55 -0400

    Comoact formularie (problem when saving)

--- resources/js/screens/organizations/Create.js
+++ resources/js/screens/organizations/Create.js
@@ -1,14 +1,26 @@
 import * as React from "react";
 import {
-  Create,
+  useCreate,
+  FormWithRedirect,
+  useTranslate,
+  Title,
   TextInput,
   SimpleForm,
   SelectInput,
   Loading,
   useNotify,
+  useCreateController,
+  CreateContextProvider,
   useRedirect
 } from 'react-admin';
+import { Typography, Grid, makeStyles, InputLabel, Box } from '@material-ui/core';
 import { useFetch } from '../../fetch';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%"
+  }
+}));
 
 const validator = (values) => {
   const errors = {};
@@ -44,37 +56,96 @@ const validator = (values) => {
   return errors;
 }
 
-const OrganizationCreate = (props) => {
+const CustomCreate = props => {
+  const [create] = useCreate('organizations');
+  const classes = useStyles();
+  const createControllerProps = useCreateController(props);
+  const {
+    defaultTitle,
+    resource
+  } = createControllerProps;
   const { isLoading, response: data } = useFetch('organizations/create');
   const notify = useNotify();
   const redirect = useRedirect();
 
-  const onSuccess = ({ data }) => {
-    notify(`¡Ha creado la institución ${data.name}!`);
-    redirect('/organizations');
-  }
+  const handleSave = React.useCallback((values) => {
+    create({
+      payload: { data: { ...values } }
+    }, {
+      onSuccess: ({ data: newRecord }) => {
+        notify(`¡Ha creado la institución ${data.name}!`);
+        redirect('/organizations');
+      }
+    })
+  }, [create, notify, redirect]);
 
+  return (
+    <CreateContextProvider value={createControllerProps}>
+      <Title title='Nueva institución' />
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={12} md={10}>
+          <Typography variant="h6" gutterBottom>
+            Nueva institución
+          </Typography>
+          { (isLoading)
+            ? <Loading loadingPrimary="Cargando..." loadingSecondary="Cargando..." />
+            : (
+            <SimpleForm validate={validator} onSave={handleSave}>
+              <div className={classes.root}>
+                <Box width={1}>
+                  <Grid container>
+                    <Grid item xs={12} sm={12} md={4}>
+                      <SelectInput source="organization_type_id" choices={data.types} label='Tipo (*)' fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                      <TextInput source="rif" label="RIF" resettable fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                      <TextInput source="name" label="Nombre" resettable fullWidth />
+                    </Grid>
+                  </Grid>
+                  <Grid container>
+                    <Grid item xs={12} sm={12} md={4}>
+                      <SelectInput source="parish_id" choices={data.parishes} label='Parroquia (*)' fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                      <SelectInput source="community_id" choices={data.communities} label='Comunidad (*)' fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={4}>
+                      <TextInput source="address" label="Dirección" resettable fullWidth />
+                    </Grid>
+                  </Grid>
+                  <Grid container>
+                    <Grid item xs={12} sm={12} md={4}>
+                      <SelectInput source="category_id" choices={data.categories} label='Sector (*)' fullWidth />
+                    </Grid>
+                  </Grid>
+                </Box>
+              </div>
+            </SimpleForm>
+          )}
+        </Grid>
+      </Grid>
+    </CreateContextProvider>
+  );
+}
+
+const OrganizationCreate = (props) => {
+
+  return <CustomCreate {...props} />;
+
+  /**
   return (
     <Create {...props} title="Nueva institución" onSuccess={onSuccess}>
       { (isLoading)
         ? <Loading loadingPrimary="Cargando..." loadingSecondary="Cargando..." />
         : (
         <SimpleForm validate={validator}>
-          <SelectInput source="organization_type_id" choices={data.types} label='Tipo (*)' />
-          <TextInput
-            source="rif"
-            label="RIF"
-            resettable
-          />
-          <TextInput source="name" label="Nombre" resettable/>
-          <SelectInput source="parish_id" choices={data.parishes} label='Parroquia (*)' />
-          <SelectInput source="community_id" choices={data.communities} label='Comunidad (*)'/>
-          <TextInput source="address" label="Dirección" resettable/>
-          <SelectInput source="category_id" choices={data.categories} label='Sector (*)'/>
         </SimpleForm>
       )}
     </Create>
   );
+   **/
 };
 
 export default OrganizationCreate;
