3e0c14d22d45676e189646ea527dbb828734a81d
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-01-25 08:57:11 -0400

    Complete styles

--- resources/js/screens/applications/MobileGrid.js
+++ resources/js/screens/applications/MobileGrid.js
@@ -1,26 +1,21 @@
 import * as React from "react";
-import { Card, CardHeader, CardContent  } from '@material-ui/core';
+import { Card, CardHeader, CardContent, ButtonBase, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  DateField,
-  EditButton,
-  NumberField,
-  TextField,
-  BooleanField,
-  useTranslate,
-  RecordMap,
-  Identifier,
-  Record,
-} from 'react-admin';
+import { useRedirect, TextField } from 'react-admin';
 import isEmpty from 'is-empty';
 import { useSelector } from 'react-redux';
 
 const useListStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    width: '100%',
+    margin: '0.5rem 0',
+  },
   card: {
     height: '100%',
+    width: '100%',
     display: 'flex',
     flexDirection: 'column',
-    margin: '0.5rem 0',
   },
   cardTitleContent: {
     display: 'flex',
@@ -39,26 +34,40 @@ const useListStyles = makeStyles(theme => ({
 
 const MobileGrid = ({ ids, data, basePath }) => {
   const classes = useListStyles();
+  const redirect = useRedirect();
 
   return (
     <div style={{ margin: '1em' }}>
       {ids.map(id => (
-        <Card key={id} className={classes.card}>
-          <CardHeader
-            title={
-              <div className={classes.cardTitleContent}>
-                <span>
-                  <TextField record={data[id]} source="title" />
-                </span>
-              </div>
-            }
-          />
-          <CardContent className={classes.cardContent}>
-            <span className={classes.cardContentRow}>
-
-            </span>
-          </CardContent>
-        </Card>
+        <ButtonBase className={classes.root} onClick={() => redirect(`applications/${id}/show`)}>
+          <Card key={id} className={classes.card}>
+            <CardHeader
+              title={
+                <div className={classes.cardTitleContent}>
+                  <span>
+                    <Typography variant="title">
+                      {data[id].title}
+                    </Typography>
+                  </span>
+                </div>
+              }
+            />
+            <CardContent className={classes.cardContent}>
+              <span className={classes.cardContentRow}>
+                <Typography variant="subtitle">Número: </Typography>&nbsp;
+                <TextField record={data[id]} source="num" />
+              </span>
+              <span className={classes.cardContentRow}>
+                <Typography variant="subtitle">Enviada: </Typography>&nbsp;
+                <TextField record={data[id]} source="created_at" />
+              </span>
+              <span className={classes.cardContentRow}>
+                <Typography variant="subtitle">Categoría: </Typography>&nbsp;
+                <TextField record={data[id]} source="category.name" />
+              </span>
+            </CardContent>
+          </Card>
+        </ButtonBase>
       ))}
     </div>
   );
